b9854cc720e45294f97bb3f8d738f5ab9b5a29a9
496090280@qq.com <ldp PC>
2022-05-26 13:54:00 +0800

    modified:formattimg code

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: daping
  * @Date: 2021-02-25 10:24:30
- * @LastEditTime: 2022-05-26 13:25:40
+ * @LastEditTime: 2022-05-26 13:51:55
  * @LastEditors: Please set LastEditors
  * @Description: 函数工具类
  * @FilePath: \undefinedd:\github\utils.js
@@ -16,24 +16,24 @@
  * @return {*}
  */
 const debounce = function (fn, delayed, immediate) {
-    let timer = null;
+  let timer = null;
 
-    return function(e) {
-        let context = this,
-            args = arguments;
-        // console.log(e)
-        if (immediate && !timer) {
-            fn.apply(context, args)
-        }
-        
-        if(timer) clearTimeout(timer);
-
-        timer = setTimeout(function () {
-            fn.apply(context, args)
-        }, delayed)
+  return function (e) {
+    let context = this,
+      args = arguments;
+    // console.log(e)
+    if (immediate && !timer) {
+      fn.apply(context, args)
     }
+
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delayed)
+  }
 }
-    
+
 /**
  * @description: 节流,每隔一段时间后执行一次，也就是降低频率
  *               通常使用场景: 滚动条事件 或者 resize 事件，通常每隔 100~500 ms执行一次即可
@@ -43,24 +43,24 @@ const debounce = function (fn, delayed, immediate) {
  * @return {*}
  */
 const throttle = function (fn, delayed, immediate) {
-    let timer = null;
-    let callNow = immediate;
+  let timer = null;
+  let callNow = immediate;
 
-    return function (e) {
-        let context = this,
-            args = arguments;
-        if (callNow) {
-            fn.apply(context, args)
-            callNow = false
-        }
-
-        if(!timer) {
-            timer = setTimeout(() => {
-                fn.apply(context, args)
-                timer = null
-            }, delayed)
-        }
+  return function (e) {
+    let context = this,
+      args = arguments;
+    if (callNow) {
+      fn.apply(context, args)
+      callNow = false
     }
+
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+        timer = null
+      }, delayed)
+    }
+  }
 }
 
 /**
@@ -74,14 +74,14 @@ function multiRequest(urls, maxNum = 5) {
   let len = urls.length
   let result = new Array(len).fill(false)
   return new Promise((resolve, reject) => {
-    while(count < maxNum) {
+    while (count < maxNum) {
       task()
     }
     function task() {
       let idx = count++
       let url = urls[idx]
       console.log(result)
-      if(idx >= len) {
+      if (idx >= len) {
         !result.includes(false) && resolve(result)
         return
       }
@@ -89,10 +89,10 @@ function multiRequest(urls, maxNum = 5) {
       fetch(url).then(res => res.json()).then(res => {
         console.log(`第${idx}个请求结束`)
         result[idx] = res
-        if(idx < len) task()
+        if (idx < len) task()
       }).catch(err => {
         result[idx] = err
-        if(idx < len) task()
+        if (idx < len) task()
       })
     }
   })
