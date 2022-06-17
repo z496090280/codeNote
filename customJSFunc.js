11f796bdf11f50797f686a1a1b1abdda2b3d82f1
496090280@qq.com <ldp PC>
2022-06-17 22:06:20 +0800

    feat: add instanceof+new

--- customJSFunc.js
+++ customJSFunc.js
@@ -1,98 +1,128 @@
 // 这是一个自定义封装js对象 文件
 
-    // 1.自定义封装 Promise,参考地址：https://juejin.cn/post/6844904094079926286
-    function Promise (fn) {
-        this.cbs = [];
+// 1.自定义封装 Promise,参考地址：https://juejin.cn/post/6844904094079926286
+function Promise(fn) {
+  this.cbs = [];
 
-        const resolve = (val) => {
-            // 注意promise的then函数需要异步执行
-            setTimeout(() => {
-                this.data = val;
+  const resolve = (val) => {
+    // 注意promise的then函数需要异步执行
+    setTimeout(() => {
+      this.data = val;
 
-                this.cbs.forEach((cb) => cb(val));
-            })
-        }
-        // 执行用户传入的函数 
-        // 并且把resolve方法交给用户执行
-        fn(resolve);
-    }
-    Promise.prototype.then = function (onResolved) {
-         // 这里叫做promise2
-        return new Promise((resolve) => {
-            this.cbs.push(() => {
-                let res = onResolved(this.data);
-
-                if(res instanceof Promise) {
-                    // user promise
-                    res.then(resolve)
-                } else {
-                    resolve(res)
-                }
-            })
-        })
-    }
-
-    // 1调用实例
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(1)
-        }, 500)
+      this.cbs.forEach((cb) => cb(val));
     })
-        .then((res) => {
-            console.log(res);
-            // user promise
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve(2)
-                }, 500)
-            })
-        })
-        .then(console.log);
+  }
+  // 执行用户传入的函数 
+  // 并且把resolve方法交给用户执行
+  fn(resolve);
+}
+Promise.prototype.then = function (onResolved) {
+  // 这里叫做promise2
+  return new Promise((resolve) => {
+    this.cbs.push(() => {
+      let res = onResolved(this.data);
 
-    // 2.自定义封装async await,参考地址：https://juejin.cn/post/6844904102053281806
-    function asyncToGenerator (generatorFunc) {
-        return function() {
-            const gen = generatorFunc.apply(this, arguments);
-            return new Promise((resolve, reject) => {
-                function step (key, arg) {
-                    let generatorResult;
+      if (res instanceof Promise) {
+        // user promise
+        res.then(resolve)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+}
 
-                    try {
-                        generatorResult = gen[key](arg);
-                    } catch (error) {
-                        reject(error);
-                    }
-                    const { value, done } = generatorResult;
-                    if(done) {
-                        return resolve(value);
-                    } else {
-                        return Promise.resolve(value).then(res => step('next', res), err => step('throw', err));
-                    }
-                    
-                }
-                step('next')
-            })
+// 1调用实例
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 500)
+})
+  .then((res) => {
+    console.log(res);
+    // user promise
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(2)
+      }, 500)
+    })
+  })
+  .then(console.log);
+
+// 2.自定义封装async await,参考地址：https://juejin.cn/post/6844904102053281806
+function asyncToGenerator(generatorFunc) {
+  return function () {
+    const gen = generatorFunc.apply(this, arguments);
+    return new Promise((resolve, reject) => {
+      function step(key, arg) {
+        let generatorResult;
+
+        try {
+          generatorResult = gen[key](arg);
+        } catch (error) {
+          reject(error);
         }
-    }
-    // 2调用实例
-        /**
-         * async的执行原理
-         * 其实就是自动执行generator函数
-         * 暂时不考虑genertor的编译步骤（更复杂）
-         */
-    function* testG () {
-        const data = yield getData();
-        console.log('data: ', data);
-        const data2 = yield getData();
-        console.log('data2: ', data2 + '2');
-        return 'success'
-    }
-    const getData = () => {
-       return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve('data')
-            }, 1000)
-        })
-    }
-    const testGAsync = asyncToGenerator(testG)
-    testGAsync().then((resule) => console.log(resule))
+        const { value, done } = generatorResult;
+        if (done) {
+          return resolve(value);
+        } else {
+          return Promise.resolve(value).then(res => step('next', res), err => step('throw', err));
+        }
+
+      }
+      step('next')
+    })
+  }
+}
+// 2调用实例
+/**
+ * async的执行原理
+ * 其实就是自动执行generator函数
+ * 暂时不考虑genertor的编译步骤（更复杂）
+ */
+function* testG() {
+  const data = yield getData();
+  console.log('data: ', data);
+  const data2 = yield getData();
+  console.log('data2: ', data2 + '2');
+  return 'success'
+}
+const getData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('data')
+    }, 1000)
+  })
+}
+const testGAsync = asyncToGenerator(testG)
+testGAsync().then((resule) => console.log(resule))
+
+function myInstanceof(obj, type) {
+  let a = obj.__proto__;
+  let b = type.prototype;
+
+  while(a !== null) {
+    if(a == b) return true
+    a = a.__proto__
+  }
+  return false
+}
+myInstanceof({}, Object)
+// true
+myInstanceof({}, String)
+// false
+
+function myNew(fn, ...args) {
+  const obj = Object.create(fn.prototype);
+  const res = fn.apply(obj, args);
+  return res instanceof Object ? res : obj;
+}
+function Person(name) {
+  this.name = name;
+}
+Person.prototype.say = function () {
+  console.log(this.name);
+}
+const p = myNew(Person, '张三');
+p.say();
+// 张三
